3c27605bacc80734e1b82ee05fa55f6ccb24b701
Alisson <alissoncorsair@hotmail.com>
2023-09-17 23:02:16 -0300

    tp 05

---
+++ TP05/Livros.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP05
+{
+	internal class Livros
+	{
+		/**
+		 * 
+		 * ------------------------------------
+| Livros                           |
+|----------------------------------|
+| - acervo: List<Livro>            |
+|----------------------------------|
+| + adicionar(Livro livro): void   |
+| + pesquisar(Livro livro): Livro  |
+------------------------------------
+		*/
+
+		
+		private List<Livro> acervo;
+
+		
+		public Livros()
+		{
+			this.acervo = new List<Livro>();
+		}
+
+		public List<Livro> Acervo { get => acervo; set => acervo = value; }
+
+		public void Adicionar(Livro livro)
+		{
+			this.acervo.Add(livro);
+		}
+
+		public Livro Pesquisar(Livro livro)
+		{
+			foreach (Livro l in this.acervo)
+			{
+				if (l.Isbn == livro.Isbn)
+				{
+					return l;
+				}
+			}
+			return null;
+		}
+
+	}
+}
